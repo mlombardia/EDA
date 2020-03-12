d9b79906d66ce6ee08c73a5f99a45cf5e486d6ff
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2020-03-12 16:33:53 -0300

    added soundex comparator, still needs final touch

--- Soundex/src/main/java/Soundex.java
+++ Soundex/src/main/java/Soundex.java
@@ -1,11 +1,12 @@
-public class Soundex {
+public abstract class Soundex {
     public static char mapping[] = {'0','1','2','3','0','1','2','0','0','2','2','4','5','5','0','1','2','6','2','3','0','1','0','2','0','2'};
     //                               A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
-    public static void main(String args[]){
 
-        char in[] = new String("INTELLIJ").toCharArray();
-        char out[] = {'0','0','0','0'};
+    private static char in[];
+    private static char out[]= {'0','0','0','0'};;
 
+    public static String encode(String word){
+        in = word.toCharArray();
         out[0]=in[0];
         int count = 1;
         char current, last = getMapping(in[0]);
@@ -16,10 +17,22 @@ public class Soundex {
                 out[count++] = current;
             }
         }
-        System.out.println(out);
+        return String.valueOf(out);
     }
 
     static char getMapping(char candidate){
         return mapping[candidate-'A'];
     }
+
+    public static int checkSimilarity(String one, String other){
+        int count = 0;
+        char aux_one[] = one.toCharArray();
+        char aux_other[] = other.toCharArray();
+        for(int i = 0; i < aux_one.length; i++){
+            if(aux_one[i] == aux_other[i])
+                count++;
+        }
+        return count;
+    }
+
 }
--- Soundex/src/main/java/SoundexTester.java
+++ Soundex/src/main/java/SoundexTester.java
@@ -1,2 +1,14 @@
 public class SoundexTester {
+
+    public static void main(String args[]){
+
+        String word = Soundex.encode("INTELLIJ");
+        System.out.println(word);
+
+        String another_word = Soundex.encode("INTACTO");
+        System.out.println(another_word);
+
+        int result = Soundex.checkSimilarity(word, another_word);
+        System.out.println(result);
+    }
 }
